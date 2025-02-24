0bcb9b7f9db047d2e079f646a35f250ad5dfc0a7
Jose-Delgadillo <a22310205@ceti.mx>
2025-02-23 18:47:17 -0600

    Prueba de ejercicio 4

--- 0004_Concatenar.py
+++ 0004_Concatenar.py
@@ -1,4 +1,4 @@
-palabra_1 = "programación "
-palabra_2 = "fácil"
-frase_completa = palabra_1 + palabra_2
-print(frase_completa)
+num_1 = "20"
+num_2 = "35"
+num_completo = num_1 + num_2
+print(num_completo)
