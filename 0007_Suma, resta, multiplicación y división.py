bb527c7878668cf059f226fee0438e87b2e99929
Jose-Delgadillo <a22310205@ceti.mx>
2025-02-23 21:46:47 -0600

    Ejemplo

---
+++ 0007_Suma, resta, multiplicación y división.py
@@ -0,0 +1,5 @@
+suma = 12 + 8 + 9
+resta = 22 - 5 - 5
+multiplicacion = 17 * 3 * 2
+division = 17 / 4 / 3
+print("Suma: " , suma ,  "\nResta: " , resta , "\nMultiplicación: " , multiplicacion , "\nDivisión: " , division)
