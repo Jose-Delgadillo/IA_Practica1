6add1b5471dae850fdc224a78a0c35a6e5af96af
Jose-Delgadillo <a22310205@ceti.mx>
2025-02-23 18:39:36 -0600

    Problema final del capitulo

--- 0002Cap2.py
+++ 0002Cap2.py
@@ -1,2 +1,4 @@
-mensaje = 'Esto es un "string"'
-print(mensaje)
+mensaje_1 = '"print()" se utiliza para imprimir valores en la consola.'
+mensaje_2 = "'print()' se utiliza para imprimir valores en la consola.' "
+print(mensaje_1)
+print(mensaje_2)
