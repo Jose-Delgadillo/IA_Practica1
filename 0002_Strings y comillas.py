8083042e60887b4550a111249e035d49af8b05bd
Jose-Delgadillo <a22310205@ceti.mx>
2025-02-23 21:48:16 -0600

    Prueba
    Cambie unos datos para probar los commit

--- 0002_Strings y comillas.py
+++ 0002_Strings y comillas.py
@@ -2,3 +2,4 @@ mensaje_1 = '"print()" se utiliza para imprimir valores en la consola.'
 mensaje_2 = "'print()' se utiliza para imprimir valores en la consola.' "
 print(mensaje_1)
 print(mensaje_2)
+print("hola")
