1be17db169816799f1946ce87f6d1dadf9da0ff8
Jose-Delgadillo <a22310205@ceti.mx>
2025-02-23 21:52:18 -0600

    Ejemplo combinar operaciones

--- 0007_Suma, resta, multiplicación y división.py
+++ 0007_Suma, resta, multiplicación y división.py
@@ -1,5 +1,2 @@
-suma = 12 + 8 + 9
-resta = 22 - 5 - 5
-multiplicacion = 17 * 3 * 2
-division = 17 / 4 / 3
-print("Suma: " , suma ,  "\nResta: " , resta , "\nMultiplicación: " , multiplicacion , "\nDivisión: " , division)
+operacion = 15 - 4 * 3 / 12 + 8
+print("Resultado: " , operacion)
