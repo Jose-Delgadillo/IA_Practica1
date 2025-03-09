11a325c42dc5532d30135aa82a0270a2d3206265
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 04:09:08 -0600

    Ejemplo de insert

---
+++ 0016_Insertar elementos con Insert.py
@@ -0,0 +1,3 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+colores.insert(1,'naranja') #puedes elegir la posicion que tú quieras para acomodar el dato en la lista y mueve el dato que reemplazaste a la derecha
+print(colores)
