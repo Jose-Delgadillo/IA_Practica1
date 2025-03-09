a2c6d439666889ee447bb54024a79ef9028df2f6
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 03:53:57 -0600

    Problema 28.
    Elimina de la siguiente lista los elementos 'amarillo' y 'rojo'. Solo puedes eliminarlos utilizando el método remove().
    colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']

--- 0013_Eliminar elementos con remove.py
+++ 0013_Eliminar elementos con remove.py
@@ -1,3 +1,4 @@
-colores = ['rojo', 'azul', 'verde', 'amarillo']
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.remove('amarillo')
 colores.remove('rojo')
 print(colores)
