0c48daa4d5fe2b6fee65ba4e8171de95f24cfce6
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 03:57:57 -0600

    Comentario en 0013, y ejemplo de 0014

--- 0013_Eliminar elementos con remove.py
+++ 0013_Eliminar elementos con remove.py
@@ -1,4 +1,4 @@
 colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
 colores.remove('amarillo')
 colores.remove('rojo')
-print(colores)
+print(colores) #se tienen que remover manualmente uno por uno ya que remove solo se activa una vez. se puede usar un bucle for, pero es un poco innecesario
---
+++ 0014_Eliminar elementos con pop.py
@@ -0,0 +1,3 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+almacena_valor = colores.pop(0)
+print('El color eliminado y almacenado es:',  almacena_valor)
