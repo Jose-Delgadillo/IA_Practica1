07a13f060c4f5cc696096a47717bee9647cc52f5
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 03:41:10 -0600

    Problema 26.
    Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista. Los colores a los que tienes que acceder son 'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
    Codigo:
    colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']

--- 0011_Posiciones negativas de listas.py
+++ 0011_Posiciones negativas de listas.py
@@ -1,3 +1,2 @@
-colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila',
-'negro', 'rosa', 'blanco', 'naranja']
-print(colores[-1])
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores[-1],colores[3],colores[-5],colores[-2],colores[0],)
