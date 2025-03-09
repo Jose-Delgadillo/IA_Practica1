7848bed77f25a06aa642fea96742135cd6fcb998
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 04:13:54 -0600

    Problema 31.
    Añade a la siguiente lista los colores 'magenta' y 'turquesa' utilizando insert(). Tendrás que posicionar 'magenta' en la posición siguiente a la de 'lila' y 'turquesa' en la penúltima posición. Deberás hacer esto utilizando posiciones de lista negativas.
    colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'

--- 0016_Insertar elementos con Insert.py
+++ 0016_Insertar elementos con Insert.py
@@ -1,3 +1,4 @@
-colores = ['rojo', 'azul', 'verde', 'amarillo']
-colores.insert(1,'naranja') #puedes elegir la posicion que tú quieras para acomodar el dato en la lista y mueve el dato que reemplazaste a la derecha
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.insert(6,'magenta')
+colores.insert(-1,'turquesa') #Aqui lo acomoda uno a la izquierda de la posicion que elegiste. No se puede poner en la ultima posicion con insert
 print(colores)
