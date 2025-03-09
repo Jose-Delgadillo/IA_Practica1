88c2fa1affe3d93b33b9cc382e9abe6e5d304d91
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 04:24:47 -0600

    Ejemplo de sort en reverse

---
+++ 0017_métodos sort() y sorted() para ordenar listas.py
@@ -0,0 +1,3 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+colores.sort(reverse=True) #puedes hacer que sea de la z-a usando reverse. (True es importante que sea con T mayúscula)
+print(colores)
