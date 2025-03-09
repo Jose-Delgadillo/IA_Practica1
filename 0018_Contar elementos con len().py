262df5fdf2f73fb8e2121c7794d6ed5e95678bf8
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 04:31:53 -0600

    Ejemplo de len

---
+++ 0018_Contar elementos con len().py
@@ -0,0 +1,4 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo','turquesa','fuxia']
+numero_colores = len(colores) #Hace que sea una variable que se adapta a cambios en la lista principal
+print(colores)
+print(numero_colores)
