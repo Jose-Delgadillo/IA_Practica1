fa9240f209a46ddce80598c6f195cffc72dd1c9d
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-09 04:03:41 -0600

    Update 0014_Eliminar elementos con pop.py

--- 0014_Eliminar elementos con pop.py
+++ 0014_Eliminar elementos con pop.py
@@ -1,3 +1,3 @@
 colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
-lista = colores.pop(1)+" y "+colores.pop(-2)
+lista = colores.pop(1)+" y "+colores.pop(-2) #agregué la y de forma estetica, realmente no es necesaria. Además esta no era la solucion, pero está mas comprimida, me gustó más lo que se me ocurrio
 print("Los colores guardados en la lista son: ",lista)
