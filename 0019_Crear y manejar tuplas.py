ed7f41f011b6db46c7ab3a51d2da1177275e6d29
Jose-Delgadillo <a22310205@ceti.mx>
2025-03-12 17:35:47 -0600

    Problema 33.
    Imprime la segunda posición de esta tupla.
    colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')

--- 0019_Crear y manejar tuplas.py
+++ 0019_Crear y manejar tuplas.py
@@ -1,2 +1,2 @@
-tupla = (10, 15, 20, 'El resultado de la operación es:')
-print(tupla[3], tupla[2] + tupla[0] * tupla[1] / tupla[0])
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(colores[1])
